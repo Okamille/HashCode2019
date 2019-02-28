4f64e47a9008b75d61ea4eb0a9c1899cff447d98
Antoine Gauthier <antoine.gauthier@ensta.fr>
2019-02-28 19:20:45 +0100

    Added

--- loader.py
+++ loader.py
@@ -10,5 +10,21 @@ def processLine(line):
     line = line.split(' ')
     orientation = line[0]
     n_tags = int(line[1])
-    tags = [line[2 + i] for i in range(n_tags)]
+    tags = [line[2 + i].rstrip('\n') for i in range(n_tags)]
     return Image(tags=tags, orientation=orientation)
+
+def findTag(tag, image_list):
+    return [containsTag(image, tag) for image in image_list]
+
+def containsTag(image, tag):
+    return tag in image.tags
+
+def load_file_dict(image_list):
+    hashtag = dict()
+    for image in image_list:
+        for tag in image.tags:
+            if tag not in hashtag.keys():
+                hashtag[tag] = [image]
+            else:
+                hashtag[tag].append(image)
+    return hashtag
