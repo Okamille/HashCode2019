c3f0a0fec9ce7db0b667b5482c21ce1377f70cbd
christopheboetto <c.boettolegrain14@eabjm.org>
2019-02-28 19:50:34 +0100

    scoring et matrice, à checker pour matrice

--- matrix_score.py
+++ matrix_score.py
@@ -10,13 +10,30 @@ import numpy as np
 
 
 def scoring(S1, S2):
-	return np.min([len(S1.tags & S2.tags), len(S1.tags - S2.tags), len(S1.tags - S2.tags)])
+	return np.min([len(S1.tags & S2.tags), len(S1.tags - S2.tags), len(S2.tags - S1.tags)])
 
 
+
+def score_matrix(list_images):
+	n = len(list_images)
+	matrix = np.zeros((n,n))
+	for i in range(n):
+		for j in range(n):
+			matrix[i,j] = scoring(list_images[i], list_images[j])
+	return matrix
 # def create_matrix(list_images):
 
 A = Image(set([0,1,2,3]), 'H')
 B = Image(set([1,2,3, 4,5]), 'H')
+C = Image(set([1,2,5]), 'H')
+D = Image(set([1,2,4,5]), 'H')
+E = Image(set([1,5]), 'H')
+
+list_images = [A,B,C,D,E]
+
+print(score_matrix(list_images))
 
 
-print(scoring(A,B))
+# print(scoring(A,B))
+
+
